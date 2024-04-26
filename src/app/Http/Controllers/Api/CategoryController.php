203a20d7f3c3fbafb6201f3d6558d9fcc78fb7ff
Marcos <marcosmariondev@gmail.com>
2024-04-25 23:01:05 -0300

    feat: return

--- src/app/Http/Controllers/Api/CategoryController.php
+++ src/app/Http/Controllers/Api/CategoryController.php
@@ -6,11 +6,12 @@ use App\Entities\CategoryEntity;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CategoryStoreRequest;
 use App\Models\Category;
+use Illuminate\Http\JsonResponse;
 
 class CategoryController extends Controller
 {
 
-    public function store(CategoryStoreRequest $request)
+    public function store(CategoryStoreRequest $request): JsonResponse
     {
         $categoryModel = Category::create($request->all());
         return response()->json(['message' => CategoryEntity::fromArray($categoryModel->toArray())],200);
